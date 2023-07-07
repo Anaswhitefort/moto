0a8f9aed7100c007a7e40e7fbfdfb240cfdc4c27
Anaswhitefort <anasmuhammedinfo@gmail.com>
2023-07-07 18:52:35 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `moto`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap"]
+};
